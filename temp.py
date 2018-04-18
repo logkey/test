c1a53fbf425f0fbcb69f6d12de226f7b34182984
logkey <hello@hello.cn>
2018-04-18 19:32:17 +0800

    111

---
+++ temp.py
@@ -0,0 +1,10 @@
+name='dog cat bird fish'
+print name.title()
+print name.upper()
+print name.lower()
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+rr="sfsfsfsfsdfsdf  wer  zfawe fz 3r  rzrrrrrrr"
+print rr.rstrip('r')
+print rr.lstrip('s')
+print rr.strip('s')
+print rr
