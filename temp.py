18af05e8c2bf1c57443bc4d2a5a8a5bbad11f117
logkey <hello@hello.cn>
2018-04-18 22:57:45 +0800

    222

--- temp.py
+++ temp.py
@@ -1,10 +1,25 @@
-name='dog cat bird fish'
-print name.title()
-print name.upper()
-print name.lower()
-print("Languages:\n\tPython\n\tC\n\tJavaScript")
-rr="sfsfsfsfsdfsdf  wer  zfawe fz 3r  rzrrrrrrr"
-print rr.rstrip('r')
-print rr.lstrip('s')
-print rr.strip('s')
-print rr
+
+
+ll=['123','312','xxfs','sd']
+
+ll.append('end')
+ll.insert(2,'22')
+del ll[-1]
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+num=[]
+for numx in range(3,31):
+    if numx%3 ==0:
+        num.append(numx)
+print num
+
+print num[0:3]
+print num[:3]
+yuanzu=('cc','1','22','333','aa')
+num.sort(reverse=True)
+zd={'name':'jack','age':8,'age':'tttt'}
+print type(zd)
+print zd['age']
